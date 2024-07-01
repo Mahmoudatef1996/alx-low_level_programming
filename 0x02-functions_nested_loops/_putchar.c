dba478f5d46d603d66dab6d490266a9f0ff19a95
mahmoudatef1996 <mahmoud.atef1996@gmail.com>
2024-07-01 04:06:46 -0700

    void print_alphabet(void) {         char ch = 'a';
            while (ch <= 'z')
            {
                    _putchar(ch);
                    ch++;
            }
            _putchar('\n');
    }

--- 0x02-functions_nested_loops/_putchar.c
+++ 0x02-functions_nested_loops/_putchar.c
@@ -11,3 +11,15 @@ int _putchar(char c)
 {
 	return (write(1, &c, 1));
 }
+
+void print_alphabet(void)
+{
+        char ch = 'a';
+
+        while (ch <= 'z')
+        {
+                _putchar(ch);
+                ch++;
+        }
+        _putchar('\n');
+}
