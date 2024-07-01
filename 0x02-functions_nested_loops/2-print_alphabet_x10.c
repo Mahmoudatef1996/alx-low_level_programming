7883aaf86d9c1398959440f022991cd2d6b52fee
mahmoudatef1996 <mahmoud.atef1996@gmail.com>
2024-07-01 04:32:09 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_alphabet_x10 - print alphapet 10 times
+ *
+ * Return: no return
+ */
+void print_alphabet_x10(void)
+{
+	int i = 0;
+	char ch = 'a';
+
+	while (i < 10)
+	{
+		while (ch <= 'z')
+		{
+			_putchar(ch);
+			ch++;
+		}
+		_putchar('\n');
+		i++
+	}
+}
