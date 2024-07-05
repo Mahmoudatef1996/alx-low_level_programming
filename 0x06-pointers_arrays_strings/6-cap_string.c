31ab23c879ace7046f81d2e5a8c642a36c152888
mahmoudatef1996 <mahmoud.atef1996@gmail.com>
2024-07-05 13:14:19 -0700

    6-cap_string.c

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,40 @@
+/**
+ * cap_string -  function that capitalizes all words of a string.
+ * * @ch: the string
+ * Return: return the srting
+ */
+char *cap_string(char *ch)
+{
+	int i;
+	int len = 0;
+
+	while (*ch != '\0')
+	{
+		if (i == 0 && *ch >= 'a' && *ch <= 'z')
+		{
+			*ch = *ch - 32;
+			i++;
+		}
+		else if (*ch == ',' || *ch == ';' || *ch == '.' || *ch == '!'
+				|| *ch == '?' || *ch == '"' || *ch == ' '
+				|| *ch == '(' || *ch == ')' || *ch == '{'
+				|| *ch == '}' || *ch == '\n' || *ch == '\t')
+		{
+			i = 0;
+		}
+		else
+		{
+			if (*ch >= 'A' && *ch <= 'Z' && i != 0)
+				*ch = *ch + 32;
+			i++;
+		}
+		ch++;
+		len++;
+	}
+	while (len > 0)
+	{
+		len--;
+		ch--;
+	}
+	return (ch);
+}
