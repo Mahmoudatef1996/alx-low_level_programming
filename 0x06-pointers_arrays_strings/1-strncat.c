e0bbbc38f22e5da7e7d5f810e57cbdb5aae81bc0
mahmoudatef1996 <mahmoud.atef1996@gmail.com>
2024-07-05 11:00:53 -0700

    fix beety

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -26,10 +26,10 @@ char *_strncat(char *dest, char *src, int n)
 	}
 	for (j = 0; j < n && src[j] != '\0'; j++)
 	{
-		dest[i+j] = src[j];
+		dest[i + j] = src[j];
 		n--;
 	}
 	if (n > 0)
-		dest[i+j] = '\0';
+		dest[i + j] = '\0';
 	return (dest);
 }
