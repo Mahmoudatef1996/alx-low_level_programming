621e6e1dfe5f01bd4584f875f53e23bbf68111a1
mahmoudatef1996 <mahmoud.atef1996@gmail.com>
2024-07-06 09:54:49 -0700

    5-strstr.c

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,35 @@
+#include <stddef.h>
+/**
+ * _strstr -  function that locates a substring
+ * @haystack: the first parameter
+ * @needle: the second parameter
+ * Return : pointer of char
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	char *start;
+	int i = 0;
+
+	while (*haystack != '\0')
+	{
+		if (*haystack == *needle)
+		{
+			start = needle;
+			needle++;
+			i++;
+			while (*needle != '\0' && *haystack != '\0' && *needle == *(haystack + 1))
+			{
+				needle++;
+				i++;
+				haystack++;
+			}
+			if (*needle == '\0')
+				return (start);
+			needle = needle - i;
+
+		}
+		haystack++;
+
+	}
+	return (NULL);
+}
