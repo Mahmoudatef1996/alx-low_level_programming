97a4fcc205d2bd7160ad652cde37f69d60362db8
mahmoudatef1996 <mahmoud.atef1996@gmail.com>
2024-07-08 06:57:02 -0700

    timetabale

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_times_table - function that prints the n times table, starting with 
+ *
+ * @n: print array
+ * Return: no return
+ */
+void print_times_table(int n)
+{
+	int i ;
+
+	for (i = 0; i < n; i ++)
+	{
+		for ( j = 0; j < n; j++)
+		{
+			printf("%d", i * j);
+			if (j == n - 1)
+				continue;
+			printf(", ");
+		}
+		_putchar('\n');
+	}
+}
