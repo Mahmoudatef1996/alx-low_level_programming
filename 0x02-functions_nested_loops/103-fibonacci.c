82ce3b2add4deb02b06ff346a52b23daec19adb5
mahmoudatef1996 <mahmoud.atef1996@gmail.com>
2024-07-08 08:02:32 -0700

    103-fibonacci.c

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/**
+ * fibonacci - Fibonacci sequence is defined
+ * @n: the fibonanci
+ * Return : no return
+ */
+void fibonacci(int n)
+{
+	long a = 1, b = 2, c;
+	int i;
+	long sum = 0;
+
+	for (i = 0; i < n; i++)
+	{
+		if (sum <= 4000000)
+			sum += a);
+		c = a + b;
+		a = b;
+		b = c;
+	}
+	printf("ld\n", sum);
+
+}
+
+/**
+ * main - Entry of the program
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	fibonacci(50);
+	return (0);
+}
