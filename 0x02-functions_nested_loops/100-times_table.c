3dce7d49d58e1973b9284571dd1dc08413fa5312
mahmoudatef1996 <mahmoud.atef1996@gmail.com>
2024-07-08 07:01:27 -0700

    void print_times_table(int n);

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -10,16 +10,17 @@ void print_times_table(int n)
 {
 	int i ;
 	int j ;
-
+	
+	if ( n >= 0 && n <=15)
 	for (i = 0; i < n; i ++)
 	{
-		for ( j = 0; j < n; j++)
+		for (j = 0; j < n; j++)
 		{
 			printf("%d", i * j);
 			if (j == n - 1)
 				continue;
 			printf(", ");
 		}
-		_putchar('\n');
+		printf("\n");
 	}
 }
