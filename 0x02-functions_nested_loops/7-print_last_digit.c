534a5a1ee151817e98996c1ae17ec12088d893f5
mahmoudatef1996 <mahmoud.atef1996@gmail.com>
2024-07-08 12:37:33 -0700

    lastfix

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,17 +8,10 @@
  */
 int print_last_digit(int n)
 {
-	if (n > 0)
-	{
-		while (n >= 10)
-			n = n % 10;
-	}
-	else if (n < 0)
-	{
-		while (n <= -10)
-			n = n % 10;
+	if (n < 0)
 		n = n * -1;
-	}
-	n = n * 10 + n;
+	while (n >= 10)
+		n = n % 10;
+	_putchar('0' + n);
 	return (n);
 }
