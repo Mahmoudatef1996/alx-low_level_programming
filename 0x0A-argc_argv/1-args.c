a805dc5e9afe34ec190107700c802ebc99fc52f1
mahmoudatef1996 <mahmoud.atef1996@gmail.com>
2024-07-28 02:46:33 -0700

    1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry of the programm
+ * @argc: argument counter
+ * @argv: arguments array
+ * Return: return 0
+ */
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc - 1);
+	(void)argv;
+	return (0);
+}
