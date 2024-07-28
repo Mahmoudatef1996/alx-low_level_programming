d007b61177f6909235b359aff67f23c3e0123c5a
mahmoudatef1996 <mahmoud.atef1996@gmail.com>
2024-07-28 07:17:52 -0700

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -2,6 +2,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
+ * slen - function that returns the len of string
+ * @s: the string
+ * Return: returns the len of string
+ */
+int slen(char *s)
+{
+	int len = 0;
+	while (*s != '\0')
+	{
+		len++;
+		s++;
+	}
+	s = s - len;
+	return (len);
+}
+/**
  * str_concat -  function that concatenates two strings.
  * @s1: the first string
  * @s2: the second string
@@ -9,56 +25,44 @@
  */
 char *str_concat(char *s1, char *s2)
 {
-	int len1 = 0;
-	int len2 = 0;
+	int len1 = slen(s1);
+	int len2 = slen(s2);
 	char *con;
 	
 	if ( s1 == NULL && s2 == NULL)
 		return (NULL);
-	while (s1 != NULL || *s1 != '\0')
-	{
-		len1++;
-		s1++;
-	}
-	while (s2 != NULL || *s2 != '\0')
-	{
-		len2++;
-		s2++;
-	}
-	s1 = s1 - len1;
-	s2 =s2 - len2;
-	len1++;
-	len2++;
 	if (s1 == NULL && s2 != NULL)
 	{
 		con = malloc(sizeof(char) * len2);
-	
+		if (con == NULL)
+			return (NULL);
 		while (*s2 != '\0')
 		{
 			*con = *s2;
 			con++;
 			s2++;
 		}
-		*con = '\0';
 		con = con - len2;
 		return (con);
 	}
 	if (s1 != NULL && s2 == NULL)
 	{
 		con = malloc(sizeof(char) * len1);
+		if (con == NULL)
+			return (NULL);
 		while (*s1 != '0')
 		{
 			*con = *s1;
 			con++;
 			s1++;
 		}
-		*con = '\0';
 		con = con - len1;
 		return (con);
-	}
+	}	
 
-	con = malloc(sizeof(char) * (len1 + len2 - 1));
-
+	con = malloc(sizeof(char) * (len1 + len2 + 1));
+	if (con == NULL)
+		return (NULL);
 	while (*s1 != '\0')
 	{
 		*con = *s1;
@@ -72,7 +76,7 @@ char *str_concat(char *s1, char *s2)
 		con++;
 	}
 	*con = '\0';
-	con = con - len1 - len2 -1;
+	con = con - len1 - len2;
 	return (con);
 
 }
