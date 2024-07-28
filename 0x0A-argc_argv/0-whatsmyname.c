c41b29cd76b1a36ea7c0a624cde9c66fb64d73c8
mahmoudatef1996 <mahmoud.atef1996@gmail.com>
2024-07-28 02:40:00 -0700

    argc

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry of the programm
+ * @argc: argument counter
+ * @argv: arguments array
+ * Return: return 0
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s \n", argv[0]);
+	return (0);
+}
