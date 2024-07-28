2719f67a96d94b422665484df07b4f2b990ef9d8
mahmoudatef1996 <mahmoud.atef1996@gmail.com>
2024-07-28 04:52:24 -0700

    0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -14,7 +14,7 @@ char *create_array(unsigned int size, char c)
 	unsigned int i;
 
 	if (size == 0)
-		return ("NULL");
+		return (NULL);
 
 	t = (char *)malloc(sizeof(char) * size);
 	for (i = 0; i < size; i++)
