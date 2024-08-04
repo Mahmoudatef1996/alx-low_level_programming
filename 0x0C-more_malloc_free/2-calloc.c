ef5b5657d6cf24d6b88d81c63300710b6996cc7a
mahmoudatef1996 <mahmoud.atef1996@gmail.com>
2024-08-04 06:37:04 -0700

    return (NULL);

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -9,11 +9,11 @@
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
+	if (nmemb == 0 || size == 0)
+		return (NULL);
 	unsigned int i = nmemb * size;
 	void *ar = malloc(i);
 
-	if (nmemb == 0 || size == 0)
-		return (NULL);
 	if (ar == NULL)
 		return (NULL);
 	for (i = 0; i < (nmemb * size); i++)
