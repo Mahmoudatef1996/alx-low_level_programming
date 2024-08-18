eda63911400582585cf86d6dd18d776e8520378e
mahmoudatef1996 <mahmoud.atef1996@gmail.com>
2024-08-18 06:41:42 -0700

    finished

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,23 +1,53 @@
 #include <stdio.h>
 #include <stdarg.h>
 #include "variadic_functions.h"
+/**
+ * print_char - function that print char
+ * @args: the args to the function
+ * Return: no return
+ */
 void print_char(va_list args)
 {
-	printf("%c",va_arg(args, int));
+	printf("%c", va_arg(args, int));
 }
+/**
+ * print_int - function to print int
+ * @args: the args
+ * Return: no return
+ */
 void print_int(va_list args)
 {
 	printf("%d", va_arg(args, int));
 }
+/**
+ * print_float - function that prints float
+ * @args: the args of fun
+ * Return: no return
+ */
 void print_float(va_list args)
 {
 	printf("%f", va_arg(args, double));
 
 }
+/**
+ * print_str - function that print string
+ * @args: the args of function
+ * Return: no return
+ */
 void print_str(va_list args)
 {
-	printf("%s", va_arg(args, char *));
+	char *c = va_arg(args, char *);
+
+	if (c)
+		printf("%s", c);
+	else
+		printf("(nil)");
 }
+/**
+ * print_all - function that prints all functions
+ * @format: the formate char
+ * Return: no return
+ */
 void print_all(const char * const format, ...)
 {
 	va_list args;
@@ -31,10 +61,11 @@ void print_all(const char * const format, ...)
 	};
 
 	va_start(args, format);
-	i = 0, x =0;
+	i = 0, x = 0;
 	while (format && format[i])
 	{
 		int j = 0;
+
 		if (i != 0 && x == 1)
 			printf(", ");
 		while (format_specs[j].specifier && format[i] != format_specs[j].specifier)
@@ -42,7 +73,7 @@ void print_all(const char * const format, ...)
 		if (format_specs[j].specifier)
 		{
 			format_specs[j].f(args);
-			x = 1; 
+			x = 1;
 			i++;
 			continue;
 		}
