549f9145487590bd111c415149e18a9da294ec01
mahmoudatef1996 <mahmoud.atef1996@gmail.com>
2024-09-01 06:08:46 -0700

    try

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * free_listint2 -  a function that frees a listint_t list
+ * @head - the first node
+ * Return: no return
+ */
+void free_listint2(listint_t **head)
+{
+	*head = NULL;
+}
