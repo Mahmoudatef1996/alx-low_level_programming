89ecfaa786b5c679756e34f00be2216470b93794
mahmoudatef1996 <mahmoud.atef1996@gmail.com>
2024-09-01 06:21:08 -0700

    fixed

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 /**
  * free_listint2 -  a function that frees a listint_t list
- * @head - the first node
+ * @head: the first node
  * Return: no return
  */
 void free_listint2(listint_t **head)
